d29da0d27bf7b74b6bfde0a8a50c82af91bf2a1a
Mark Fowler <mark@twoshortplanks.com>
2012-06-19 10:44:58 -0400

    fix missing newline in =head NAME

--- lib/WWW/Testafy.pm
+++ lib/WWW/Testafy.pm
@@ -3,7 +3,9 @@ package WWW::Testafy;
 
 our $VERSION = 0.01;
 
-=head1 NAME WWW::Testafy - Testafy API for perl developers
+=head1 NAME
+
+WWW::Testafy - Testafy API for perl developers
 
 =head1 SYNOPSIS
 
